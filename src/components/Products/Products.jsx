f31f96026463398eb973d1cef9b44d2ed9db8caa
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-12-16 07:28:11 -0800

    Add the initial version of the app with Products demo and React dependencies setup

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
+
+const products = [
+  {
+    id: 1,
+    name: 'Shoes',
+    description: 'Running shoes.',
+    price: '$10',
+    image:
+      'https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
+  },
+  {
+    id: 2,
+    name: 'Coat',
+    description: 'Good for winter',
+    price: '$20',
+    image:
+      'https://images.unsplash.com/photo-1539533018447-63fcce2678e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+  }
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify='center' spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
