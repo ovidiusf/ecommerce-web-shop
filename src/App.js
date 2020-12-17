088a3a8b18d50bc1bf801a9399393142bb36feb1
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-12-17 12:13:03 -0800

    Add the Cart component- wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
   // use the useState hook
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -14,17 +15,32 @@ const App = () => {
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    // retrieve the cart and set it to the state
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
   // use the useEffect hook
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
+
+  // console.log(products);
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
+      <Cart cart={cart}></Cart>
     </div>
   );
 };
